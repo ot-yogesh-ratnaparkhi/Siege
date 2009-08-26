1d1b589916f320d77a821e916c34157caeefcaca
Marcus Bratton <marcus.bratton@gmail.com>
2009-08-25 19:22:49 -0700

    Checkin of additional files

---
+++ Siege.Container.NinjectAdapter/NinjectServiceLocator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Ninject;
+using Ninject.Parameters;
+using Ninject.Planning.Bindings;
+using Siege.ServiceLocation;
+
+namespace Siege.Container.NinjectAdapter
+{
+    public class NinjectServiceLocator : IContextualServiceLocator
+    {
+        private readonly IKernel kernel;
+
+        public NinjectServiceLocator(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public T GetInstance<T>()
+        {
+            return this.kernel.Get<T>();
+        }
+
+        public T GetInstance<T>(Type type)
+        {
+            return (T)this.kernel.Get(type);
+        }
+
+        public T GetInstance<T>(string name)
+        {
+            return this.kernel.Get<T>(name);
+        }
+
+        public void Register<T>(IUseCase<T> useCase)
+        {
+            BindingBuilder<T> builder = new BindingBuilder<T>(new Binding(typeof(T)));
+
+            if (useCase is GenericUseCase<T>)
+            {
+                GenericUseCase<T> genericCase = useCase as GenericUseCase<T>;
+
+                genericCase.Bind(this.kernel, builder);
+            }
+
+            if (useCase is ImplementationUseCase<T>)
+            {
+                var implementation = useCase as ImplementationUseCase<T>;
+
+                implementation.Bind(this.kernel, builder);
+            }
+        }
+
+        public T GetInstance<T, TContext>(TContext context) where TContext : IContext
+        {
+            return this.kernel.Get<T>(new ConstructorArgument("", context.GetValue()));
+        }
+    }
+
+    public static class GenericUseCaseExtensions
+    {
+        public static void Bind<TBaseType>(this GenericUseCase<TBaseType> useCase, IKernel kernel, BindingBuilder<TBaseType> builder)
+        {
+            Type type = useCase.GetBinding();
+
+            builder.To(type).InTransientScope();
+            kernel.AddBinding(builder.Binding);
+        }
+    }
+
+    public static class ImplementationUseCaseExtensions
+    {
+        public static void Bind<TBaseType>(this ImplementationUseCase<TBaseType> useCase, IKernel kernel, BindingBuilder<TBaseType> builder)
+        {
+            builder.ToConstant(useCase.GetBinding()).InTransientScope();
+            kernel.AddBinding(builder.Binding);
+        }
+    }
+}
---
+++ UnitTests/SiegeContainerTests.cs
@@ -0,0 +1,78 @@
+﻿using Ninject;
+using NUnit.Framework;
+using Siege.Container;
+using Siege.Container.NinjectAdapter;
+using Siege.ServiceLocation;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class SiegeContainerTests
+    {
+        protected IKernel kernel;
+        protected IContextualServiceLocator locator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            kernel = new StandardKernel();
+
+            locator = new SiegeContainer(new NinjectServiceLocator(kernel));
+        }
+        [Test]
+        public void Should_Be_Able_To_Bind_An_Interface_To_A_Type()
+        {
+            locator.Register(Given<ITestInterface>.Then<TestCase1>());
+
+            Assert.IsTrue(locator.GetInstance<ITestInterface>() is TestCase1);
+        }
+
+        [Test]
+        public void Should_Be_Able_To_Bind_An_Interface_To_A_Type_Based_On_Rule()
+        {
+            locator.Register(Given<ITestInterface>.When<Context<TestContext>>(context => context.Value.TestCases == TestEnum.Case2).Then<TestCase2>());
+
+            Assert.IsTrue(locator.GetInstance<ITestInterface, Context<TestContext>>(CreateContext(TestEnum.Case2)) is TestCase2);
+        }
+
+        [Test]
+        public void Should_Use_Rule_When_Satisfied()
+        {
+            locator.Register(Given<ITestInterface>.Then<TestCase1>());
+            locator.Register(Given<ITestInterface>.When<Context<TestContext>>(context => context.Value.TestCases == TestEnum.Case2).Then<TestCase2>());
+
+            Assert.IsTrue(locator.GetInstance<ITestInterface, Context<TestContext>>(CreateContext(TestEnum.Case2)) is TestCase2);
+        }
+
+
+        [Test]
+        public void Should_Not_Use_Rule_When_Not_Satisfied()
+        {
+            locator.Register(Given<ITestInterface>.Then<TestCase1>());
+            locator.Register(Given<ITestInterface>.When<Context<TestContext>>(context => context.Value.TestCases == TestEnum.Case2).Then<TestCase2>());
+
+            Assert.IsTrue(locator.GetInstance<ITestInterface, Context<TestContext>>(CreateContext(TestEnum.Case3)) is TestCase1);
+        }
+
+        private Context<TestContext> CreateContext(TestEnum types)
+        {
+            return new Context<TestContext> { Value = new TestContext { TestCases = types } };
+        }
+    }
+
+    public class TestContext
+    {
+        public TestEnum TestCases { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        Case1,
+        Case2,
+        Case3
+    }
+
+    public interface ITestInterface {}
+    public class TestCase1 : ITestInterface {}
+    public class TestCase2 : ITestInterface {}
+}
