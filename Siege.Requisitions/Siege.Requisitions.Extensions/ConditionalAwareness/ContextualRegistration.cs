e0eb33ab5448074df99cc56e47845def29f42f93
MarcusTheBold <marcus.bratton@gmail.com>
2010-08-26 19:11:48 -0500

    Signed-off-by: MarcusTheBold <marcus.bratton@gmail.com>

---
+++ Siege.Requisitions/Siege.Requisitions.Extensions/ConditionalAwareness/ContextualRegistration.cs
@@ -0,0 +1,47 @@
+﻿/*   Copyright 2009 - 2010 Marcus Bratton
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+
+using System;
+using Siege.Requisitions.InternalStorage;
+using Siege.Requisitions.Registrations;
+using Siege.Requisitions.RegistrationTemplates;
+
+namespace Siege.Requisitions.Extensions.ConditionalAwareness
+{
+    public class ContextualRegistration<T> : TypedRegistration
+    {
+        private readonly Func<IInstanceResolver, T> func;
+
+        public ContextualRegistration(Func<IInstanceResolver, T> func) : base(typeof(T))
+        {
+            this.func = func;
+        }
+
+        public override bool IsValid(IServiceLocatorStore context)
+        {
+            return false;
+        }
+
+        public override object GetMappedTo()
+        {
+            return this.func;
+        }
+
+        public override IRegistrationTemplate GetRegistrationTemplate()
+        {
+            return new ContextualRegistrationTemplate();
+        }
+    }
+}
---
+++ Siege.Requisitions/Siege.Requisitions.UnitTests/ContextAwarenessTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Siege.Requisitions.Extensions.ConditionalAwareness;
+using Siege.Requisitions.Extensions.ExtendedRegistrationSyntax;
+using Siege.Requisitions.UnitTests.TestClasses;
+
+namespace Siege.Requisitions.UnitTests
+{
+    public partial class ServiceLocatorTests
+    {
+        [Test, Ignore]
+        public void ShouldFindContextWithoutExplicitlyAdding()
+        {
+            locator
+                .Register(Awareness.Of(serviceLocator => serviceLocator.GetInstance<ISomeService>()))
+                .Register(Given<ISomeService>.Then<SomeService>())
+                .Register(Given<ITestInterface>.When<ISomeService>(i => i.SomeMethod()).Then<TestCase1>())
+                .Register(Given<ITestInterface>.Then<TestCase2>());
+
+            Assert.IsInstanceOf<TestCase1>(locator.GetInstance<ITestInterface>());
+        }
+
+        public interface ISomeService
+        {
+            bool SomeMethod();
+        }
+
+        public class SomeService : ISomeService
+        {
+            public bool SomeMethod()
+            {
+                return true;
+            }
+        }
+    }
+}
